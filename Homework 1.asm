e088b5f29e442102683daa921c0f32d19d87702a
Andrew Rosiclair <arosiclair@gmail.com>
2014-09-16 23:40:57 -0400

    all conversions are included though are not working completely

--- Homework 1.asm
+++ Homework 1.asm
@@ -5,6 +5,7 @@
 .text
 .globl main
 main:
+
 	la $a0,prompt	#Load the prompt string for printing
 	
 	li $v0,4		#Print the prompt
@@ -14,8 +15,10 @@ main:
 	syscall
 	
 	la $t0,($v0)	#Load the result into a temp address
+	b twos			#call the twos label
 
-	#--------------------------TWO'S COMPLEMENT REPRESENTATIONS---------------------------------
+twos:	#--------------------------TWO'S COMPLEMENT REPRESENTATIONS---------------------------------
+
 	li $v0,4		
 	la $a0,twosLabel
 	syscall
@@ -41,7 +44,10 @@ main:
 	la $a0, ($t0)
 	syscall			#Print the integer representation again.
 	
-	#--------------------------ONE'S COMPLEMENT REPRESENTATIONS---------------------------------
+	b ones
+	
+ones:	#--------------------------ONE'S COMPLEMENT REPRESENTATIONS---------------------------------
+
 	la $a0,onesLabel	#Print one's complement representation
 	li $v0, 4	
 	syscall
@@ -51,7 +57,33 @@ main:
 	li $v0,4		
 	la $a0,tab
 	syscall
-	#bltz $t0,decrement
+	
+	bltz $t0,onesNeg	#Branch to either onesNeg or onesPos depending on if the input is positive or negative
+	b onesPos
+	
+onesNeg:
+	addi $t0,$t0,-1	#decrement the input
+	li $v0,34		#Print hex representation
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall
+	li $v0,35		#Print binary representation
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall
+	li $v0,1
+	la $a0, ($t0)
+	syscall			#Print the 1's complement integer representation again.
+	addi $t0,$t0,1	#increment input
+	
+	b signMag
+	
+onesPos:
+	
 	li $v0,34		#Print hex representation
 	la $a0,($t0)
 	syscall
@@ -67,9 +99,11 @@ main:
 	li $v0,100
 	la $a0, ($t0)
 	syscall			#Print the 1's complement integer representation again.
-	#bltz $t0,increment
 	
-	#--------------------------SIGNED MAGNITUDE REPRESENTATIONS---------------------------------
+	b signMag
+	
+signMag:	#--------------------------SIGNED MAGNITUDE REPRESENTATIONS---------------------------------
+
 	la $a0,signLabel	#Print the Signed Magnitude label
 	li $v0,4
 	syscall
@@ -79,31 +113,78 @@ main:
 	li $v0,4		
 	la $a0,tab
 	syscall
-	neg $t0,$t0		#Negate the integer
-	la $a0,($t0)
+	#and $t1,$t0,$t0 #AND the input
+	neg $t1,$t0		#Negate the new integer
+	addi $t1,$t1,1		#increment the neg number
+	la $a0,($t1)
 	li $v0,34		#Print hex repesentation
 	syscall
 	li $v0,4
 	la $a0,tab
 	syscall
 	li $v0,35		#Print binary representation
+	la $a0,($t1)
 	syscall
 	li $v0,4
 	la $a0,tab
 	syscall
-	neg $t0,$t0		#Negate the integer again
+	#neg $t0,$t0		#Negate the integer again
+	#addi $t0,$t0,1	#increment input
 	li $v0,101		#Print SM integer representation
+	la $a0,($t0)
+	syscall
+	
+	b negTwos
+
+negTwos:
+
+	li $v0,4	#Print Negative Two's Label
+	la $a0,negTwosLabel
+	syscall
+	li $v0,1	#Negate the input value and print
+	neg $t0,$t0	
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall	
+	li $v0,34	#Print the hex representation
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall	
+	li $v0,35	#Print the binary representation
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall
+	li $v0,1 #Print the integer again
+	la $a0,($t0)
+	syscall
+	
+	b sixBitTwos
+	
+sixBitTwos:
+	
+	li $v0,4	#16-bit twos complement label
+	la $a0,sixteenTwos
+	syscall
+	li $v0,102	#Print 16-bit two's integer representation
+	la $a0,($t0)
+	syscall
+	li $v0,4
+	la $a0,tab
+	syscall
+	li $v0,34	#Print the hex representation
 	la $a0,($t0)
 	syscall
 	
 	li $v0,10		#Exit
 	syscall
 	
-decrement:
-	addi $t0,$t0,-1
-increment:
-	addi $t0,$t0,1
-
+	
 .data
 	prompt: .asciiz "Enter an integer.\n"
 	tab: .asciiz "\t"
@@ -112,5 +193,5 @@ increment:
 	twosLabel: .asciiz "\n2's complement:\t"
 	onesLabel: .asciiz "\n1's complement:\t"
 	signLabel: .asciiz "\nSign Magnitude:\t"
-	negTwosLabel: .asciiz "\nNeg 2's complement:\t"
+	negTwosLabel: .asciiz "\nNeg 2's complement: "
 	sixteenTwos: .asciiz "\n16-bit 2's comp:\t"
