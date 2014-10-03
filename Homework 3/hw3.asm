5d9d0bc849717f7490b029b9ffa7a06a3c0dd795
arosiclair <arosiclair@gmail.com>
2014-10-03 01:59:57 -0400

    Completed part 1. Can now convert  to radians and calculate arc length

--- Homework 3/hw3.asm
+++ Homework 3/hw3.asm
@@ -4,12 +4,32 @@
 
 #Converts a given angle argument from degrees to radians.
 toRadians:
-	move $f0,$a0	#Save a copy of the angle in degrees to $t0
-	la $f4,PI		#Load $t1 with the pi constant
-	li $f6,180
-	div.s $f8,$f4,$f6	#Save a double precision quotient in $f0
-	mul.s $
+
+	la $t0,pi
+	la $t1,den
+	l.s $f4,0($t0)		#Load $f5 with the pi constant
+	l.s $f6,0($t1)		#Load $f6 with the denominator, 180
+	div.s $f8, $f4, $f6		#Save a single precision quotient in $f0
+	mul.s $f0, $f12, $f8	#Multiply the argument by our quotient and save the result to be returned in $
 	
+	jr $ra		#return to $ra
+	
+#Finds the arclength of a given angle in degrees and float distance
+arcLength:
+
+	la $fp,($sp)
+	addi $sp,$sp,-4	#Shift the stack pointer down a word to fit $ra
+	sw $ra,($fp)	#Store the original $ra because we will be calling another function    
+	jal toRadians	#Convert the degrees in $f12 to radians in $f0
+	
+	mul.s $f0,$f0,$f13	#Multiply the radian result from the "toRadians" call by the second argument.
+	
+	lw $ra,($fp)	#Restore the return address we saved.
+	jr $ra		#Return to $ra
+
+
+.data
+	den: .float 180
 	
 	
 
