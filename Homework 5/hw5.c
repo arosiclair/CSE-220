835d4f19a74f1a5c9d6a9fc2bf85fa7de350b827
Andrew Rosiclair <arosiclair@gmail.com>
2014-10-23 16:17:03 -0400

    Fixed all errors in HW5

--- Homework 5/hw5.c
+++ Homework 5/hw5.c
@@ -25,7 +25,7 @@ int promptForId(void) {
 		/* Read in the whole line */
 		fgets(line, sizeof(line), stdin);
 		/* If we were not able to parse the string, tell the user */
-		if(!(valid = sscanf(line, "%d", &id))) {
+		if(!(valid = sscanf(line, "%d", &id))) {brea
 			printf("Invalid id entered.\n");
 		}
 	} while(!valid);
@@ -34,7 +34,7 @@ int promptForId(void) {
 
 /* Scramble the SBUID */
 int scrambleId(int id) {
-	int scrambled = id, i, r, rounds;
+	int scrambled = id, i = 0, r = 0, rounds = 0;
 	/* Seeds and generates a random number */
 	srand(id);
 	r = rand();
@@ -42,14 +42,14 @@ int scrambleId(int id) {
 	rounds = (rand() % 10) + 1;
 	printf("Performing %d rounds of shifting and xoring.\n", rounds);
 	/* Go through some crazy shifting operations */
-	for(i = 0; i < rounds; i++);
+	for(i = 0; i < rounds; i++)
 		scrambled = shift(scrambled, r >>= 1);
 	return scrambled;
 }
 
 /* Perform bitwise XOR on the SBU ID */
 int xor(int v, int id) {
-	int result;
+	int result = 0;
 	result = v ^ id;
 	return result;
 }
@@ -57,11 +57,11 @@ int xor(int v, int id) {
 /* Shift the value right and xor */
 int shift(int v, int r) {
 	int shamt = 0x08000000;
-	int result;
+	int result = 0;
 	/* while shamt is still not zero */
 	while(shamt) {
 		result += xor(v, r); /* add the return value of xor to result */
-		shamt >>= 1;	 /* Shift right by 1 */
+		shamt = shamt >> 1;	 /* Shift right by 1 */
 	}
 	return result;
 }
@@ -73,9 +73,10 @@ int printResults(int original, int scrambled) {
 	printf("%d -> %d\n", original, scrambled);
 
 	isEven = scrambled % 2; 
-	if((isEven = 0))
+	if(isEven == 0){
 		printf("The scrambled result is even!\n");
 		return 0;
+	}
 	/* else it is odd */
 	printf("The scrambled result is odd!\n");
 	return 127;
@@ -96,15 +97,31 @@ int main() {
 
 /* @@GDB_OUTPUT_HERE@@
 
+sparky% gcc -g CSE220/hw5.c -o hw5
+sparky% gdb hw5
+GNU gdb (GDB) 7.8
+Copyright (C) 2014 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.  Type "show copying"
+and "show warranty" for details.
+This GDB was configured as "sparc-sun-solaris2.10".
+Type "show configuration" for configuration details.
+For bug reporting instructions, please see:
+<http://www.gnu.org/software/gdb/bugs/>.
+Find the GDB manual and other documentation resources online at:
+<http://www.gnu.org/software/gdb/documentation/>.
+For help, type "help".
+Type "apropos word" to search for commands related to "word"...
 Reading symbols from hw5...done.
 (gdb) clear
 No source file specified.
-(gdb) break 22
-Breakpoint 1 at 0x10c08: file CSE220/hw5.c, line 22.
-(gdb) break 40
-Breakpoint 2 at 0x10d18: file CSE220/hw5.c, line 40.
-(gdb) break 47
-Breakpoint 3 at 0x10d54: file CSE220/hw5.c, line 47.
+(gdb) break 46
+Breakpoint 1 at 0x10d04: file CSE220/hw5.c, line 46.
+(gdb) break 28
+Breakpoint 2 at 0x10c08: file CSE220/hw5.c, line 28.
+(gdb) break 53
+Breakpoint 3 at 0x10d64: file CSE220/hw5.c, line 53.
 (gdb) run
 Starting program: /export/home2/a/r/arosicla/hw5 
 [Thread debugging using libthread_db enabled]
@@ -112,35 +129,76 @@ Starting program: /export/home2/a/r/arosicla/hw5
 Please enter your SBUID: 109235970
 [Switching to Thread 1 (LWP 1)]
 
-Breakpoint 1, promptForId () at CSE220/hw5.c:22
-22                      if(!(valid = sscanf(line, "%d", &id))) {
+Breakpoint 2, promptForId () at CSE220/hw5.c:28
+28                      if(!(valid = sscanf(line, "%d", &id))) {
 (gdb) print line
-$1 = "109235970\n", '\000' <repeats 46 times>, "\377\377\377\377\377\377\377\377\377:\000\000\377\277\373\320\377:@L\000\000\000\f\000\001\000\064\377\277\377\331\377\377\377\377\000\000\000\000\377\277\374\274\000\000\000\002\377\377\377\377\377\377\377\377\377\277\373\330\377:X\350\000\000\000\000\000\000\000\000\377\277\375\b\000\000\000\000\000\000\000\001\000\001\005\v\377\067\np\377 \267\200\000\001\005\v\377\067\004 \377\067\004 \000\000\000\002\006\212\317\004\000\000\000\021\000\001\003L\025\001\377\377\000\001\000\000\000\000\000\000\000\000\000\002\377\277\373\320"...
+$1 = "109235970\n", '\000' <repeats 46 times>, "\377\377\377\377\377\377\377\377\377:\000\000\377\277\373\320\377:@L\000\000\000\f\000\001\000\064\377\277\377\330\377\377\377\377\000\000\000\000\377\277\374\274\000\000\000\002\377\377\377\377\377\377\377\377\377\277\373\330\377:X\350\000\000\000\000\000\000\000\000\377\277\375\b\000\000\000\000\000\000\000\001\000\001\005\v\377\067\np\377 \267\200\000\001\005\v\377\067\004 \377\067\004 \000\000\000\002\006\212\317\004\000\000\000\021\000\001\003L\025\001\377\377\000\001\000\000\000\000\000\000\000\000\000\002\377\277\373\320"...
 (gdb) continue
 Continuing.
 SBUID: 109235970
 Performing 5 rounds of shifting and xoring.
 
-Breakpoint 2, scrambleId (id=109235970) at CSE220/hw5.c:40
-40                      scrambled = shift(scrambled, r >>= 1);
+Breakpoint 1, scrambleId (id=109235970) at CSE220/hw5.c:46
+46                      scrambled = shift(scrambled, r >>= 1);
+(gdb) print i
+$2 = 0
+(gdb) continue
+Continuing.
+
+Breakpoint 3, xor (v=109235970, id=7976) at CSE220/hw5.c:53
+53              result = v ^ id;
+(gdb) backtrace
+#0  xor (v=109235970, id=7976) at CSE220/hw5.c:53
+#1  0x00010db4 in shift (v=109235970, r=7976) at CSE220/hw5.c:63
+#2  0x00010d20 in scrambleId (id=109235970) at CSE220/hw5.c:46
+#3  0x00010eac in main () at CSE220/hw5.c:91
 (gdb) info frame
-Stack level 0, frame at 0xffbffbe8:
- pc = 0x10d18 in scrambleId (CSE220/hw5.c:40); saved pc = 0x10e80
- called by frame at 0xffbffc58
+Stack level 0, frame at 0xffbffb10:
+ pc = 0x10d64 in xor (CSE220/hw5.c:53); saved pc = 0x10db4
+ called by frame at 0xffbffb78
  source language c.
- Arglist at 0xffbffbe8, args: id=109235970
- Locals at 0xffbffbe8, Previous frame's sp in fp
+ Arglist at 0xffbffb10, args: v=109235970, id=7976
+ Locals at 0xffbffb10, Previous frame's sp in fp
  Saved registers:
-  l0 at 0xffbffbe8, l1 at 0xffbffbec, l2 at 0xffbffbf0, l3 at 0xffbffbf4,
-  l4 at 0xffbffbf8, l5 at 0xffbffbfc, l6 at 0xffbffc00, l7 at 0xffbffc04,
-  i0 at 0xffbffc08, i1 at 0xffbffc0c, i2 at 0xffbffc10, i3 at 0xffbffc14,
-  i4 at 0xffbffc18, i5 at 0xffbffc1c, fp at 0xffbffc20, i7 at 0xffbffc24
-(gdb) clear xor
+  l0 at 0xffbffb10, l1 at 0xffbffb14, l2 at 0xffbffb18, l3 at 0xffbffb1c,
+  l4 at 0xffbffb20, l5 at 0xffbffb24, l6 at 0xffbffb28, l7 at 0xffbffb2c,
+  i0 at 0xffbffb30, i1 at 0xffbffb34, i2 at 0xffbffb38, i3 at 0xffbffb3c,
+  i4 at 0xffbffb40, i5 at 0xffbffb44, fp at 0xffbffb48, i7 at 0xffbffb4c
+(gdb) clear 53
 Deleted breakpoint 3 
 (gdb) continue
 Continuing.
-109235970 -> -1249671751
-[Inferior 1 (process 20408    ) exited normally]
 
+Breakpoint 1, scrambleId (id=109235970) at CSE220/hw5.c:46
+46                      scrambled = shift(scrambled, r >>= 1);
+(gdb) print i
+$3 = 1
+(gdb) continue
+Continuing.
+
+Breakpoint 1, scrambleId (id=109235970) at CSE220/hw5.c:46
+46                      scrambled = shift(scrambled, r >>= 1);
+(gdb) print i
+$4 = 2
+(gdb) continue
+Continuing.
+
+Breakpoint 1, scrambleId (id=109235970) at CSE220/hw5.c:46
+46                      scrambled = shift(scrambled, r >>= 1);
+(gdb) print i
+$5 = 3
+(gdb) continue
+Continuing.
+
+Breakpoint 1, scrambleId (id=109235970) at CSE220/hw5.c:46
+46                      scrambled = shift(scrambled, r >>= 1);
+(gdb) print i
+$6 = 4
+(gdb) continue
+Continuing.
+109235970 -> -42303928
+The scrambled result is even!
+[Inferior 1 (process 15476    ) exited normally]
+(gdb) quit
 
 @@GDB_OUTPUT_HERE@@ */
